3b2de99c554b945d9130c29e7cdd761929462b17
Warai777 <rodneysanchez1234@gmail.com>
2025-05-18 23:39:15 -0400

    Delete winrate_calculator.py

--- winrate_calculator.py
+++
@@ -1,8 +0,0 @@
-def calculate_win_rate(char1_stats, char2_stats):
-    # Placeholder logic
-    if "FTL" in char1_stats and "Hypersonic" in char2_stats:
-        return "Char1 wins 9/10"
-    elif "Universal" in char1_stats and "Planetary" in char2_stats:
-        return "Char1 wins 10/10"
-    else:
-        return "Even matchup. Needs more data."
