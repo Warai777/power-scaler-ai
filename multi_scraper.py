8c75dc6fa67aedc79b2bdfe03d70853ef3e3ed52
Warai777 <rodneysanchez1234@gmail.com>
2025-05-18 23:39:47 -0400

    Delete multi_scraper.py

--- multi_scraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def scrape_vs_battle_wiki(character):
-    url = f"https://vsbattles.fandom.com/wiki/{character.replace(' ', '_')}"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    return soup.get_text()
